f7624a76c21819096e473b9a89559d7681f15ca9
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-02-13 16:04:48 -0300

    Bootstrap icons. PortfolioService
    Agregamos iconos de bootstrap. Implementamos el servicio PortfolioService en el componente skills, con una tabla de base, como prueba.

--- src/app/Components/skills/skills.component.ts
+++ src/app/Components/skills/skills.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Skills } from 'src/app/Models/skills/skills.module';
+import { PortfolioService } from 'src/app/Services/portfolio.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
 
+export class SkillsComponent implements OnInit {
+  lista: any = [];
+  skillActual: Skills = {id:"", tarea: "", porcentaje: "" };
+  ruta: any = "tareas"; 
+  id: string = "";
+  
+
+  // switch: boolean = false;
+
+  constructor(private portfolioService: PortfolioService, private router: Router) { };
+
+  ngOnInit(): void {
+    this.listarSkills();
+  }
+
+
+  listarSkills() {
+    this.portfolioService.getElements(this.ruta).subscribe(
+      res => {
+        this.lista = res;
+      console.log(this.lista)},
+      err => console.log(err)
+    )
+  }
+  // listarSkills() {
+  //   this.portfolioService.getElements(this.ruta).subscribe(
+  //     res => { this.lista = res; },
+  //     err => console.log(err),
+  //   );
+  // }
+
+  eliminar(id: string) {
+    this.portfolioService.deleteElemento(this.ruta, id).subscribe(
+    res => { this.ngOnInit() },
+    err=>console.log(err)
+   );
+ }
+
+  // inputEditar(id: Tarea) {
+  //   this.switch = !this.switch;
+  // }
+  
 }
+
--- src/app/Services/url-base.service.ts
+++ src/app/Services/url-base.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UrlBaseService {
-  // urlBase: any = "http://localhost:8080/api/";
+urlBase: any = "http://localhost:8080/api/";
 
   constructor() { }
 }
