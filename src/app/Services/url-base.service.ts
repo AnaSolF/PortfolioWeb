5c5b25baa445b54a6553570ccb4318f42ae39586
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-14 00:36:21 -0300

    Cambio

--- src/app/Services/url-base.service.ts
+++ src/app/Services/url-base.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UrlBaseService {
-  urlBase: any = "https://proyectoback.onrender.com/";
+  urlBase: any = "https://proyectoback.onrender.com/api/";
 
   constructor() { }
 }
